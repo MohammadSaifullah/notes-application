1db6f67253d7c8ad0096c7ad7b157e3ae60a4e84
Mohammad Saifullah <saifullah@isquarebs.in>
2021-03-12 15:41:26 +0530

    notes-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-
 })
 export class AppComponent {
-
   title = 'notes2';
-  titles!: string;
+  titles!: Object;
   file: Array<any> = [];
   folder: Array<any> = [];
-
+  noteTitle:any
   bodies!: string;
   folderName: any;
   folderstore: any;
@@ -24,11 +22,8 @@ export class AppComponent {
     if (b) {
       this.file = JSON.parse(b);
     }
-
-
   }
   delete(i: any) {
-
     this.file.splice(i, 1);
     localStorage.setItem("note", JSON.stringify(this.file));
     console.table(this.file)
@@ -39,32 +34,31 @@ export class AppComponent {
       noteBody: this.bodies
     }
     this.file.push(x);
-    localStorage.setItem("file", JSON.stringify(this.file));
 
+
+    localStorage.setItem("file", JSON.stringify(this.file));
   }
   clear() {
     localStorage.clear();
-
   }
   addFolder() {
-
     let p = {
       fold: prompt("Enter the folder name:")
     };
     this.folder.push(p);
+    console.log(this.folder)
     localStorage.setItem("folder", JSON.stringify(this.folder));
     const a = localStorage.getItem("folder");
     if (a) {
       this.folder = JSON.parse(a)
     }
-
   }
   openFile() {
-
-    const b = localStorage.getItem("file");
-    if (b) {
-      this.file = JSON.parse(b);
-    }
+      const b = localStorage.getItem("file");
+      if (b) {
+        this.file = JSON.parse(b);
+      }
+    
   }
 
 }
